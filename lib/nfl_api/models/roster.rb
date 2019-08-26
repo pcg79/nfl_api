31628cd38b64b1d71bd864475bce2d1079fcd441
Pat George <patrick.george@freeagent.com>
2019-08-26 11:14:01 +0100

    Implemented Roster.by_team_and_season endpoint

--- lib/nfl_api/models/roster.rb
+++ lib/nfl_api/models/roster.rb
@@ -24,8 +24,14 @@ module NFLApi
       @team_players = team_players
     end
 
-    def self.by_team(team)
-      team = Team.by_team(team)
+    # We need the team id to get their roster.  So theoretically if we
+    # receive a team id we shouldn't need to do an extra API call to
+    # get the team.  But the /roster endpoint returns an abbreviated
+    # version of the Team.  So we'll just get the team from the
+    # /team endpoint in both cases so we have the full team data.
+    #
+    def self.by_team(team_id_or_name)
+      team = Team.by_team(team_id_or_name)
 
       json_data = parse_json(endpoint(team.id))
 
@@ -35,7 +41,17 @@ module NFLApi
       new season, team, team_players
     end
 
-    def self.season(season_number)
+    def self.by_team_and_season(team_id_or_name, season_year)
+      return nil if season_year.to_i < NFLApi::NFL_START_YEAR
+
+      team = Team.by_team_and_season(team_id_or_name, season_year)
+
+      json_data = parse_json(season_specific_endpoint(team.id, season_year))
+
+      season = json_data["season"]
+      team_players = player_array(json_data["teamPlayers"])
+
+      new season, team, team_players
     end
 
     private
@@ -50,7 +66,8 @@ module NFLApi
       "http://www.nfl.com/feeds-rs/roster/#{team_id}.json"
     end
 
-    def self.season_specific_endpoint(season_number)
+    def self.season_specific_endpoint(team_id, season_year)
+      "http://www.nfl.com/feeds-rs/roster/#{team_id}/#{season_year}.json"
     end
   end
 end
